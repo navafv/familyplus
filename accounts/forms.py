41365a65ca64e26f37191ff2281c13a8fcdda89b
Navaf V <navafkcp@gmail.com>
2025-10-05 19:15:25 +0530

    django authenticaton

---
+++ accounts/forms.py
@@ -0,0 +1,24 @@
+from django import forms
+from .models import Account
+
+class RegistrationForm(forms.ModelForm):
+    password = forms.CharField()
+    confirm_password = forms.CharField()
+    class Meta:
+        model = Account
+        fields = ['first_name', 'last_name', 'phone_number', 'email', 'password']
+
+    def clean(self):
+        cleaned_data = super(RegistrationForm, self).clean()
+        password = cleaned_data.get('password')
+        confirm_password = cleaned_data.get('confirm_password')
+
+        if password != confirm_password:
+            raise forms.ValidationError(
+                "Password does not match!"
+            )
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs.update({'class': 'w-full p-3 border rounded-md'})
